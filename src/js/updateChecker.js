6ba53b6ffa649b65692fe864c1770a0096198848
NStephenson <nickstephenson90@gmail.com>
2017-11-27 21:20:18 -0500

    started update checker

---
+++ src/js/updateChecker.js
@@ -0,0 +1,23 @@
+export default class updateChecker {
+
+  constructor(){}
+
+  checkVersion(){
+    if(){//current version < new version
+      //display popup window
+    }
+  }
+
+  createUpdatedDiv(){
+    let div = document.createElement('div')
+
+  }
+
+}
+
+
+// <div>
+//   <h2>LE3 Has been Updated!!</h2>
+
+//   To see the changelog, click <a href="changelog link">here</a>
+// </div>
